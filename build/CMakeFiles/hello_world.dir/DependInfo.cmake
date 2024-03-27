182fd148f091777616de95b6edc8ff34ec84e8a3
Kudl1k <stepanek.kudlacek@gmail.com>
2024-03-27 11:40:08 +0100

    git done

---
+++ build/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "hello_world_autogen/timestamp" "custom" "hello_world_autogen/deps"
+  "/Users/stepankudlacek/VSCode/qttests/blog-vscode-template/build/hello_world_autogen/mocs_compilation.cpp" "CMakeFiles/hello_world.dir/hello_world_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/hello_world.dir/hello_world_autogen/mocs_compilation.cpp.o.d"
+  "/Users/stepankudlacek/VSCode/qttests/blog-vscode-template/src/main.cpp" "CMakeFiles/hello_world.dir/src/main.cpp.o" "gcc" "CMakeFiles/hello_world.dir/src/main.cpp.o.d"
+  "/Users/stepankudlacek/VSCode/qttests/blog-vscode-template/src/widget.cpp" "CMakeFiles/hello_world.dir/src/widget.cpp.o" "gcc" "CMakeFiles/hello_world.dir/src/widget.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/hello_world_autogen.dir/ParseCache.txt"
+  "hello_world_autogen"
+  "CMakeFiles/hello_world.dir/hello_world_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/hello_world.dir/hello_world_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/hello_world.dir/src/main.cpp.o"
+  "CMakeFiles/hello_world.dir/src/main.cpp.o.d"
+  "CMakeFiles/hello_world.dir/src/widget.cpp.o"
+  "CMakeFiles/hello_world.dir/src/widget.cpp.o.d"
+  "hello_world.app/Contents/MacOS/hello_world"
+  "hello_world.pdb"
+  "hello_world_autogen/mocs_compilation.cpp"
+  "hello_world_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
